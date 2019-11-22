68d4c8947ad59f10f71540baf9ac1ea3aa9a5a93
Chet <chethana96@gmail.com>
2019-11-23 12:18:18 +1300

    Complete API table for Buttons

--- src/pages/components/buttons.tsx
+++ src/pages/components/buttons.tsx
@@ -41,10 +41,49 @@ export default ({ location }: ButtonPageProps) => (
             options: 'primary, secondary, tertiary, ghost, danger, disabled',
             default: 'primary',
           },
-          { prop: '', desc: '', type: '', options: '', default: '' },
-          { prop: '', desc: '', type: '', options: '', default: '' },
-          { prop: '', desc: '', type: '', options: '', default: '' },
-          { prop: '', desc: '', type: '', options: '', default: '' },
+          {
+            prop: 'shape',
+            desc: 'The shape of the button',
+            type: 'string',
+            options: 'rounded, pills, circle',
+            default: 'rounded',
+          },
+          {
+            prop: 'loading',
+            desc: 'Whether to show a spinner or not',
+            type: 'boolean',
+            options: '-',
+            default: 'false',
+          },
+          {
+            prop: 'disabled',
+            desc: 'Whether the button is inactive',
+            type: 'boolean',
+            options: '-',
+            default: 'false',
+          },
+          {
+            prop: 'icon',
+            desc: 'The icon to go alongside the button contents',
+            type: 'ReactNode',
+            options: '-',
+            default: 'null',
+          },
+          {
+            prop: 'showLoadingContent',
+            desc:
+              'Whether to show specified text when the button is in a loading state',
+            type: 'boolean',
+            options: '-',
+            default: 'false',
+          },
+          {
+            prop: 'loadingContent',
+            desc: 'The content to show while the button is in a loading state',
+            type: 'ReactNode | string',
+            options: '-',
+            default: 'null',
+          },
         ]}
       />
     </ComponentLayout>
