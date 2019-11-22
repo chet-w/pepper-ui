e66b56b5fcdbad6cac4ea22bb4178543865aeea5
Chet <chethana96@gmail.com>
2019-11-23 12:33:20 +1300

    Add code highlighting to API table

--- src/components/APITable.tsx
+++ src/components/APITable.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const StyledTable = styled.table``
+const StyledTable = styled.table`
+  & code {
+    background: ${props => `${props.theme.palette.primary}22`};
+    padding: 2px 4px;
+    color: ${props => props.theme.palette.primary};
+  }
+`
 
 interface Props {
   header: string[]
@@ -12,7 +18,7 @@ interface APITableRow {
   prop: string
   desc: string
   type: string
-  options: string
+  options: string[]
   default: string
 }
 
@@ -28,9 +34,24 @@ const APITable: React.FC<Props> = ({ header, data }) => (
       <tbody>
         {data.map(row => (
           <tr>
-            {Object.values(row).map(col => (
-              <td>{col}</td>
-            ))}
+            {Object.values(row).map((col, index) =>
+              index === 1 ? (
+                <td>{col}</td>
+              ) : index === 3 ? (
+                <td>
+                  {col.map((option: string, optIndex: number) => (
+                    <>
+                      <code>{option}</code>
+                      {optIndex !== col.length - 1 && ', '}
+                    </>
+                  ))}
+                </td>
+              ) : (
+                <td>
+                  <code>{col}</code>
+                </td>
+              )
+            )}
           </tr>
         ))}
       </tbody>
